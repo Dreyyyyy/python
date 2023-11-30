a3b5c28e5a34146df8b5f1edd967f16f4eaf4d1c
Andrey <andreydias@alunos.utfpr.edu.br>
2023-11-30 12:06:59 -0300

    img_viewer added

--- tkinter/img_viewer.py
+++ tkinter/img_viewer.py
@@ -1,27 +1,57 @@
 from tkinter import Tk, Button, Label
 from PIL import ImageTk, Image
 
+
+def change_photo(img_list, opt, img_label):
+    global i
+
+    if opt == ">>" and i < len(img_list) - 1:
+        i += 1
+    elif opt == "<<" and i > 0:
+        i -= 1
+
+    img_label.config(image=img_list[i])
+
+
+def show_ui(root, img_list):
+    img_label = Label(root, image=img_list[i])
+    img_label.grid(row=0, column=0, columnspan=3)
+
+    foward_button = Button(
+        root,
+        text=">>",
+        command=lambda: change_photo(img_list, ">>", img_label),
+    )
+    backward_button = Button(
+        root,
+        text="<<",
+        command=lambda: change_photo(img_list, "<<", img_label),
+    )
+    exit_button = Button(root, text="Quit", command=root.quit)
+
+    foward_button.grid(row=1, column=2)
+    backward_button.grid(row=1, column=0)
+    exit_button.grid(row=1, column=1)
+
+
 root = Tk()
 
-img0 = Image.open("/home/shuruyi/Documents/git/python/tkinter/aux_files/img0.jpg")
-img1 = Image.open("/home/shuruyi/Documents/git/python/tkinter/aux_files/img1.jpg")
-img2 = Image.open("/home/shuruyi/Documents/git/python/tkinter/aux_files/img2.jpg")
-img3 = Image.open("/home/shuruyi/Documents/git/python/tkinter/aux_files/img3.webp")
-img4 = Image.open("/home/shuruyi/Documents/git/python/tkinter/aux_files/img4.webp")
-img5 = Image.open("/home/shuruyi/Documents/git/python/tkinter/aux_files/img5.webp")
+i = 0
 
-img_list = [ImageTk.PhotoImage(img) for img in [img0, img1, img2, img3, img4, img5]]
+img_paths = [
+    "/home/shuruyi/Documents/git/python/tkinter/aux_files/img0.jpg",
+    "/home/shuruyi/Documents/git/python/tkinter/aux_files/img1.jpg",
+    "/home/shuruyi/Documents/git/python/tkinter/aux_files/img2.jpg",
+    "/home/shuruyi/Documents/git/python/tkinter/aux_files/img3.webp",
+    "/home/shuruyi/Documents/git/python/tkinter/aux_files/img4.webp",
+    "/home/shuruyi/Documents/git/python/tkinter/aux_files/img5.webp",
+]
 
-img_label = Label(image=img_list[0])
-foward_button = Button(root, text=">>")
-backward_button = Button(root, text="<<")
-exit_button = Button(root, text="Quit", command=root.quit)
+# Resize images to a uniform size
+target_size = (800, 600)
+img_list = [
+    ImageTk.PhotoImage(Image.open(path).resize(target_size)) for path in img_paths
+]
 
-img_label.grid(row=0, column=0, columnspan=3)
-foward_button.grid(row=1, column=2)
-backward_button.grid(row=1, column=0)
-exit_button.grid(row=1, column=1)
-
-exit_button.grid()
-
+show_ui(root, img_list)
 root.mainloop()
